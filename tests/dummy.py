9def3e1b0aef2f2413c8c9d448467b8f67a50d57
Dan Killinger <git@dank.anonaddy.com>
2022-11-30 18:24:30 -0800

    fix CI and mixin code not being added

--- tests/dummy.py
+++ tests/dummy.py
@@ -16,20 +16,20 @@ class TEST(Structure):
         return f"TEST = {self.one}, {self.two}, {self.ptr}"
 
 
-my_os = dummy.os()
-proc_info = my_os.process_info_list()[0]
-proc = my_os.process_from_info(proc_info)
+def test_ghetto():
+    my_os = dummy.os()
+    proc_info = my_os.process_info_list()[0]
+    proc = my_os.process_from_info(proc_info)
 
-# Test writing new `TEST` structure.
-test_struct = TEST((1, 2), 2, POINTER64(POINT)(proc_info.address + 0x7777))
-proc.write(proc_info.address, TEST, test_struct)
-proc.write(test_works.ptr.addr, POINT, POINT(55, 3.14))
+    # Test writing new `TEST` structure.
+    test_struct = TEST((1, 2), 2, POINTER64(POINT)(proc_info.address + 0x7777))
+    proc.write(proc_info.address, TEST, test_struct)
+    proc.write(proc_info.address + 0x7777, POINT, POINT(55, 3.14))
 
-# Test reading a structure.
-test_works = proc.read(proc_info.address, TEST)
-print(f"test does work: {test_works}")
+    # Test reading a structure.
+    test_works = proc.read(proc_info.address, TEST)
+    assert test_works.two == 2
 
-
-# Test reading through a pointer.
-point_works = proc.read_ptr(test_works.ptr)
-print(f"point through a pointer: {point_works}")
+    # Test reading through a pointer.
+    point_works = proc.read_ptr(test_works.ptr)
+    assert point_works.x == 55
