a5f61e836db2788ed1f06c7fcbc23c94efaa0ce2
Dan Killinger <git@dank.anonaddy.com>
2022-12-16 18:51:03 -0800

    refactor internal datatype repr
    fixes linux and mac having incorrectly sized types, breaking CI

--- src/internal.rs
+++ src/internal.rs
@@ -1,4 +1,4 @@
-use std::ffi::{self, c_double, c_float};
+use std::ffi::*;
 use std::mem::size_of;
 
 use indexmap::IndexMap;
@@ -11,8 +11,14 @@ use crate::MemflowPyError;
 /// Please stick to explicit widths, no c_int nonsense!
 #[derive(Clone, Debug)]
 pub enum InternalDT {
+    /// Represents the C signed char datatype, and interprets the value as small integer. The constructor accepts an optional integer initializer; no overflow checking is done.
+    Byte,
+    /// Represents the C unsigned char datatype, it interprets the value as small integer. The constructor accepts an optional integer initializer; no overflow checking is done.
+    UByte,
     /// Represents the C char datatype, and interprets the value as a single character. The constructor accepts an optional string initializer, the length of the string must be exactly one character.
     Char,
+    /// Represents the C wchar_t datatype, and interprets the value as a single character unicode string. The constructor accepts an optional string initializer, the length of the string must be exactly one character.
+    WideChar,
     /// Represents the C double datatype. The constructor accepts an optional float initializer.
     Double,
     /// Represents the C long double datatype. The constructor accepts an optional float initializer. On platforms where sizeof(long double) == sizeof(double) it is an alias to c_double.
@@ -20,24 +26,22 @@ pub enum InternalDT {
     LongDouble,
     /// Represents the C float datatype. The constructor accepts an optional float initializer.
     Float,
-    /// Represents the C 8-bit signed int datatype. Usually an alias for c_byte.
-    Int8,
-    /// Represents the C 16-bit signed int datatype. Usually an alias for c_short.
-    Int16,
-    /// Represents the C 32-bit signed int datatype. Usually an alias for c_int.
-    Int32,
-    /// Represents the C 64-bit signed int datatype. Usually an alias for c_longlong.
-    Int64,
-    /// Represents the C 8-bit unsigned int datatype. Usually an alias for c_ubyte.
-    UInt8,
-    /// Represents the C 16-bit unsigned int datatype. Usually an alias for c_ushort.
-    UInt16,
-    /// Represents the C 32-bit unsigned int datatype. Usually an alias for c_uint.
-    UInt32,
-    /// Represents the C 64-bit unsigned int datatype. Usually an alias for c_ulonglong.
-    UInt64,
-    /// Represents the C wchar_t datatype, and interprets the value as a single character unicode string. The constructor accepts an optional string initializer, the length of the string must be exactly one character.
-    WideChar,
+    /// Represents the C signed short datatype. The constructor accepts an optional integer initializer; no overflow checking is done.
+    Short,
+    /// Represents the C unsigned short datatype. The constructor accepts an optional integer initializer; no overflow checking is done.
+    UShort,
+    /// Represents the C signed int datatype. The constructor accepts an optional integer initializer; no overflow checking is done. On platforms where sizeof(int) == sizeof(long) it is an alias to c_long.
+    Int,
+    /// Represents the C unsigned int datatype. The constructor accepts an optional integer initializer; no overflow checking is done. On platforms where sizeof(int) == sizeof(long) it is an alias for c_ulong.
+    UInt,
+    /// Represents the C signed long datatype. The constructor accepts an optional integer initializer; no overflow checking is done.
+    Long,
+    /// Represents the C unsigned long datatype. The constructor accepts an optional integer initializer; no overflow checking is done.
+    ULong,
+    /// Represents the C signed long long datatype. The constructor accepts an optional integer initializer; no overflow checking is done.
+    LongLong,
+    /// Represents the C unsigned long long datatype. The constructor accepts an optional integer initializer; no overflow checking is done.
+    ULongLong,
     /// Native pointer type.
     Pointer(PyObject),
     /// Backed by the python function `POINTER32`.
@@ -53,23 +57,25 @@ pub enum InternalDT {
 impl InternalDT {
     pub fn py_from_bytes(&self, mut bytes: Vec<u8>) -> crate::Result<PyObject> {
         Python::with_gil(|py| match self {
+            InternalDT::Byte => Ok(i8::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::UByte => Ok(u8::from_le_bytes(bytes[..].try_into()?).to_object(py)),
             InternalDT::Char => Ok(PyBytes::new(py, &bytes).to_object(py)),
-            InternalDT::Double => {
-                Ok(ffi::c_float::from_le_bytes(bytes[..].try_into()?).to_object(py))
-            }
-            InternalDT::LongDouble => todo!(),
-            InternalDT::Float => {
-                Ok(ffi::c_float::from_le_bytes(bytes[..].try_into()?).to_object(py))
-            }
-            InternalDT::Int8 => Ok(i8::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::Int16 => Ok(i16::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::Int32 => Ok(i32::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::Int64 => Ok(i64::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::UInt8 => Ok(u8::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::UInt16 => Ok(u16::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::UInt32 => Ok(u32::from_le_bytes(bytes[..].try_into()?).to_object(py)),
-            InternalDT::UInt64 => Ok(u64::from_le_bytes(bytes[..].try_into()?).to_object(py)),
             InternalDT::WideChar => todo!(),
+            InternalDT::Double => Ok(c_double::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::LongDouble => todo!(),
+            InternalDT::Float => Ok(c_float::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::Short => Ok(c_short::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::UShort => Ok(c_ushort::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::Int => Ok(c_int::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::UInt => Ok(c_uint::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::Long => Ok(c_long::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::ULong => Ok(c_ulong::from_le_bytes(bytes[..].try_into()?).to_object(py)),
+            InternalDT::LongLong => {
+                Ok(c_longlong::from_le_bytes(bytes[..].try_into()?).to_object(py))
+            }
+            InternalDT::ULongLong => {
+                Ok(c_ulonglong::from_le_bytes(bytes[..].try_into()?).to_object(py))
+            }
             InternalDT::Pointer(_) => {
                 todo!("Need to store address in an attribute to access later")
             }
@@ -104,23 +110,25 @@ impl InternalDT {
 
     pub fn py_to_bytes(&self, obj: PyObject) -> crate::Result<Vec<u8>> {
         Python::with_gil(|py| match self {
+            InternalDT::Byte => Ok(obj.extract::<i8>(py)?.to_le_bytes().to_vec()),
+            InternalDT::UByte => Ok(obj.extract::<u8>(py)?.to_le_bytes().to_vec()),
             InternalDT::Char => Ok(PyBytes::try_from_exact(obj.as_ref(py))
                 .unwrap()
                 .as_bytes()
                 .to_owned()),
+            // OS widechar encoding.
+            InternalDT::WideChar => todo!(),
             InternalDT::Double => Ok(obj.extract::<c_double>(py)?.to_le_bytes().to_vec()),
             InternalDT::LongDouble => todo!(),
             InternalDT::Float => Ok(obj.extract::<c_float>(py)?.to_le_bytes().to_vec()),
-            InternalDT::Int8 => Ok(obj.extract::<i8>(py)?.to_le_bytes().to_vec()),
-            InternalDT::Int16 => Ok(obj.extract::<i16>(py)?.to_le_bytes().to_vec()),
-            InternalDT::Int32 => Ok(obj.extract::<i32>(py)?.to_le_bytes().to_vec()),
-            InternalDT::Int64 => Ok(obj.extract::<i64>(py)?.to_le_bytes().to_vec()),
-            InternalDT::UInt8 => Ok(obj.extract::<u8>(py)?.to_le_bytes().to_vec()),
-            InternalDT::UInt16 => Ok(obj.extract::<u16>(py)?.to_le_bytes().to_vec()),
-            InternalDT::UInt32 => Ok(obj.extract::<u32>(py)?.to_le_bytes().to_vec()),
-            InternalDT::UInt64 => Ok(obj.extract::<u64>(py)?.to_le_bytes().to_vec()),
-            // OS widechar encoding.
-            InternalDT::WideChar => todo!(),
+            InternalDT::Short => Ok(obj.extract::<c_short>(py)?.to_le_bytes().to_vec()),
+            InternalDT::UShort => Ok(obj.extract::<c_ushort>(py)?.to_le_bytes().to_vec()),
+            InternalDT::Int => Ok(obj.extract::<c_int>(py)?.to_le_bytes().to_vec()),
+            InternalDT::UInt => Ok(obj.extract::<c_uint>(py)?.to_le_bytes().to_vec()),
+            InternalDT::Long => Ok(obj.extract::<c_long>(py)?.to_le_bytes().to_vec()),
+            InternalDT::ULong => Ok(obj.extract::<c_ulong>(py)?.to_le_bytes().to_vec()),
+            InternalDT::LongLong => Ok(obj.extract::<c_longlong>(py)?.to_le_bytes().to_vec()),
+            InternalDT::ULongLong => Ok(obj.extract::<c_ulonglong>(py)?.to_le_bytes().to_vec()),
             InternalDT::Pointer(_) => todo!(),
             InternalDT::Pointer32(_) => Ok(obj
                 .getattr(py, "addr")?
@@ -159,14 +167,23 @@ impl InternalDT {
 
     pub fn size(&self) -> usize {
         match self {
-            InternalDT::Double => size_of::<ffi::c_double>(),
-            // TODO: I have a feeling this wont end well.
-            InternalDT::LongDouble => size_of::<ffi::c_double>() * 2,
-            InternalDT::Float => size_of::<ffi::c_float>(),
-            InternalDT::Char | InternalDT::Int8 | InternalDT::UInt8 => 1,
-            InternalDT::Int16 | InternalDT::UInt16 | InternalDT::WideChar => 2,
-            InternalDT::Int32 | InternalDT::UInt32 | InternalDT::Pointer32(_) => 4,
-            InternalDT::Int64 | InternalDT::UInt64 | InternalDT::Pointer64(_) => 8,
+            InternalDT::Byte => size_of::<c_schar>(),
+            InternalDT::UByte => size_of::<c_uchar>(),
+            InternalDT::Char => size_of::<c_char>(),
+            InternalDT::WideChar => size_of::<c_char>() * 2,
+            InternalDT::Short => size_of::<c_short>(),
+            InternalDT::UShort => size_of::<c_ushort>(),
+            InternalDT::Double => size_of::<c_double>(),
+            InternalDT::LongDouble => size_of::<c_double>() * 2,
+            InternalDT::Float => size_of::<c_float>(),
+            InternalDT::Int => size_of::<c_int>(),
+            InternalDT::UInt => size_of::<c_uint>(),
+            InternalDT::Long => size_of::<c_long>(),
+            InternalDT::ULong => size_of::<c_ulong>(),
+            InternalDT::LongLong => size_of::<c_longlong>(),
+            InternalDT::ULongLong => size_of::<c_ulonglong>(),
+            InternalDT::Pointer32(_) => 4,
+            InternalDT::Pointer64(_) => 8,
             InternalDT::Pointer(_) => size_of::<usize>(),
             InternalDT::Array(_, dt, len) => dt.size() * (*len as usize),
             InternalDT::Structure(_, dts) => dts.iter().map(|(_, dt)| dt.size()).sum(),
@@ -188,21 +205,23 @@ impl TryFrom<PyObject> for InternalDT {
                 let name: String =
                     Python::with_gil(|py| value.getattr(py, "__name__")?.extract(py))?;
                 let dt = match name.as_str() {
+                    "c_byte" => Self::Byte,
+                    "c_ubyte" | "c_bool" => Self::UByte,
                     "c_char" => Self::Char,
                     "c_char_p" => todo!("add c_char_p support"),
+                    "c_wchar" => Self::WideChar,
+                    "c_wchar_p" => todo!("add c_wchar_p support"),
                     "c_double" => Self::Double,
                     "c_longdouble" => Self::LongDouble,
                     "c_float" => Self::Float,
-                    "c_int8" | "c_byte" => Self::Int8,
-                    "c_int16" | "c_short" => Self::Int16,
-                    "c_int32" | "c_long" | "c_int" => Self::Int32,
-                    "c_int64" | "c_longlong" => Self::Int64,
-                    "c_uint8" | "c_bool" | "c_ubyte" => Self::UInt8,
-                    "c_uint16" | "c_ushort" => Self::UInt16,
-                    "c_uint32" | "c_ulong" | "c_uint" => Self::UInt32,
-                    "c_uint64" | "c_ulonglong" => Self::UInt64,
-                    "c_wchar" => Self::WideChar,
-                    "c_wchar_p" => todo!("add c_wchar_p support"),
+                    "c_short" => Self::Short,
+                    "c_ushort" => Self::UShort,
+                    "c_int" => Self::Int,
+                    "c_uint" => Self::UInt,
+                    "c_long" => Self::Long,
+                    "c_ulong" => Self::ULong,
+                    "c_longlong" => Self::LongLong,
+                    "c_ulonglong" => Self::ULongLong,
                     name => unreachable!("unknown SimpleCData type: {}", name),
                 };
                 Ok(dt)
